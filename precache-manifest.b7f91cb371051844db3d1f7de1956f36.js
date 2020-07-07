facea9b7df967ecfb0dd5bf6ba7b5c5521141c27
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-07-08 08:59:44 +0900

    Updates

---
+++ precache-manifest.b7f91cb371051844db3d1f7de1956f36.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c068a52c7bbb117911a4fe3047e6cc8e",
+    "url": "/appscreen_eval/index.html"
+  },
+  {
+    "revision": "4944d9dfbcd4bad335c1",
+    "url": "/appscreen_eval/static/css/2.14e53b4e.chunk.css"
+  },
+  {
+    "revision": "3949e8ba7286a2eb9183",
+    "url": "/appscreen_eval/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4944d9dfbcd4bad335c1",
+    "url": "/appscreen_eval/static/js/2.306e82e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/appscreen_eval/static/js/2.306e82e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3949e8ba7286a2eb9183",
+    "url": "/appscreen_eval/static/js/main.719cb173.chunk.js"
+  },
+  {
+    "revision": "246c99c64fd31bebf9f3",
+    "url": "/appscreen_eval/static/js/runtime-main.b5014ad8.js"
+  },
+  {
+    "revision": "62e6d1b10e89baa5a64fd7e071fc5ef7",
+    "url": "/appscreen_eval/static/media/Profile.62e6d1b1.jpg"
+  },
+  {
+    "revision": "b5ad2b1338814978c4e7088853f6b160",
+    "url": "/appscreen_eval/static/media/back.b5ad2b13.png"
+  }
+]);
