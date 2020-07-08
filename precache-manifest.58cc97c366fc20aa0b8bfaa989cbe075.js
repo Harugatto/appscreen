f50c1da4241deebdc23715bbbccfc7500c9787db
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-07-08 10:48:56 +0900

    Updates

---
+++ precache-manifest.58cc97c366fc20aa0b8bfaa989cbe075.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156f32748bb5bf71e8c2cd2628816867",
+    "url": "/appscreen/index.html"
+  },
+  {
+    "revision": "71b66c3f1ae9c789ecd9",
+    "url": "/appscreen/static/css/2.14e53b4e.chunk.css"
+  },
+  {
+    "revision": "c6922f7aaa0392d96860",
+    "url": "/appscreen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71b66c3f1ae9c789ecd9",
+    "url": "/appscreen/static/js/2.a42c6b95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/appscreen/static/js/2.a42c6b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6922f7aaa0392d96860",
+    "url": "/appscreen/static/js/main.8eded23a.chunk.js"
+  },
+  {
+    "revision": "381f0538fd77ab719955",
+    "url": "/appscreen/static/js/runtime-main.7b95431c.js"
+  },
+  {
+    "revision": "62e6d1b10e89baa5a64fd7e071fc5ef7",
+    "url": "/appscreen/static/media/Profile.62e6d1b1.jpg"
+  },
+  {
+    "revision": "b5ad2b1338814978c4e7088853f6b160",
+    "url": "/appscreen/static/media/back.b5ad2b13.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/appscreen_eval/precache-manifest.b7f91cb371051844db3d1f7de1956f36.js"
+  "/appscreen/precache-manifest.58cc97c366fc20aa0b8bfaa989cbe075.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/appscreen_eval/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/appscreen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
